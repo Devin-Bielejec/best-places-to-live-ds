d9f6209af7f77093ce94ba80f82fcc5ba028ec33
Devin Bielejec <devin.bielejec@gmail.com>
2023-12-15 18:08:03 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify, render_template, send_file
 from flask_cors import CORS
+import os 
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
